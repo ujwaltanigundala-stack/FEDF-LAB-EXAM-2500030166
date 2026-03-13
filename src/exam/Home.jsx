96e04137d5ad57b49e1e4e6f919ff5c35ff568c9
ujwal <2500030166@kluniversity.in>
2026-03-13 12:05:16 +0530

    modified Home.jsx

--- src/exam/Home.jsx
+++ src/exam/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             <h2>I am in Home Page</h2>
             <img src='https://thumbs.dreamstime.com/b/events-13644351.jpg?w=992' alt='home' width={500} height={300}/>
             <p>
-                This project is about the event management system.
+               <h2><b> This project is about the event management system.</b></h2>
             </p>
     </div>
   )
